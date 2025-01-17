6d07aaab994f36cefc4c3f4af403cc74de7042d3
cosmoDiv <cosmoD3v@gmail.com>
2025-01-17 19:19:15 +0800

    created section header component

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,28 @@
+interface SectionHeaderProps {
+  title: string;
+  eyebrow: string;
+  description: string;
+}
+const SectionHeader: React.FC<SectionHeaderProps> = ({
+  title,
+  eyebrow,
+  description,
+}) => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+          {eyebrow}
+        </p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6 ">
+        {title}
+      </h2>
+      <p className="text-center md:text-lg lg:text-xl max-w-md text-white/60 mt-4 mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
+
+export default SectionHeader;
